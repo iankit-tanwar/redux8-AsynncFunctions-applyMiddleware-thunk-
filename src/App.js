810422d093373b72e2f842074384ed89f8c224c9
Ankit Tanwar <tankitofficial01@gmail.com>
2023-12-20 11:00:56 +0530

    applymiddleware

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { asynFun } from '.';
 
 function App() {
+
+  let state = useSelector(state=>state)
+
+
+   const dispatch = useDispatch()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <h1>{state.name}</h1>
+
+     <button onClick={()=>{dispatch(asynFun())}}>Change my name</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+
+
+
+let rootReducer = (preState, action) => {
+  let newState = preState;
+
+  switch(action.type){
+    case 'A':
+    return {
+      ...newState,
+      name : "Tanwar"
+    }
+    break;
+
+
+  }
+
+
+  return newState;
+}
+
+let initialState = {
+  name: 'Ankit'
+}
+
+let actionCreator =()=>{
+  return {
+    type:'A'
+  }
+}
+
+export const asynFun =()=>{
+ console.log('okkk')
+  return (dispatch)=>{
+    setTimeout(function(){
+      return dispatch(actionCreator())
+    },5000)
+    
+  }
+}
+
+
+let store = legacy_createStore(rootReducer, initialState , applyMiddleware(thunk))
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
